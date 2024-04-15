1b155da0e74d30771b241da78b951e16ccd25057
Lokesh <lokeshsingh7695@gmail.com>
2024-04-15 15:18:45 +0530

    Added Proper Spacing between cards

--- src/components/imageslider/ImageCard.js
+++ src/components/imageslider/ImageCard.js
@@ -4,7 +4,7 @@ const ImageCard = (props) => {
     const {locations,imagesrc} = props;
   return (
     <div className="card5 w-30 relative">
-    <img src={"https://source.unsplash.com/random?"+imagesrc} alt="" className='w-[320px] h-[300px]' />
+    <img src={"https://source.unsplash.com/random?"+imagesrc} alt="" className='w-[350px] h-[300px]' />
     <div className="info  absolute bottom-2 left-4">
     <p className='text-white font-bold my-1 text-xl'>{locations}</p>
     <button className='bg-orange-600 p-2 rounded-md text-white '>View Jobs</button>
